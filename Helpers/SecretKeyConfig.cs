78ed7086b5b1ae23a2bcdb9ad242675a520c61ac
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-20 20:15:01 -0300

    Retorno da chave em bytes

--- Helpers/SecretKeyConfig.cs
+++ Helpers/SecretKeyConfig.cs
@@ -1,7 +1,13 @@
+using System.Text;
+
 namespace SGFBackend.Helpers
 {
     public class SecretKeyConfig
     {
         public string SecretKey { get; set; }
+        public byte[] SecretKeyBytes 
+        {
+            get => Encoding.ASCII.GetBytes(SecretKey);
+        }
     }
 }
