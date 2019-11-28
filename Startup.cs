dbf270c9b03a493258f1f5fea9d6e3190291a1a6
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-28 17:39:43 -0300

    Configurações do Startup

--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using SGFBackend.Entities;
+using SGFBackend.Helpers;
 
 namespace SGFBackend
 {
@@ -25,21 +30,42 @@ namespace SGFBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var secretKeyConfig = Configuration.Get<SecretKeyConfig>(); 
+            var databaseConfig = Configuration.GetSection("DB").Get<DatabaseConfig>(); 
+            var key = secretKeyConfig.SecretKeyBytes; 
+ 
+            // Possibilita DI da classe de configuração 
+            services.Configure<SecretKeyConfig>(Configuration); 
+            // Acesso dos dados no contexto (user_id) 
+            services.AddHttpContextAccessor(); 
             services.AddControllers();
+            services.AddAuthentication(x =>  
+            { 
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+            }) 
+            .AddJwtBearer(x => 
+            { 
+                x.RequireHttpsMetadata = false; 
+                x.SaveToken = true; 
+                x.TokenValidationParameters = new TokenValidationParameters 
+                { 
+                    ValidateIssuerSigningKey = true, 
+                    IssuerSigningKey = new SymmetricSecurityKey(key), 
+                    ValidateIssuer = false, 
+                    ValidateAudience = false 
+                }; 
+            }); 
+            services.AddDbContext<SgfContext>(options =>  
+                options.UseMySql(databaseConfig.ConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
